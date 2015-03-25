8bbcf9a5aef4ed503938e66aaaf537a77d7332c6
ali_m91 <ali_m91@live.com>
2015-03-25 22:37:19 +0200

    Calendar finished

---
+++ app/src/main/java/lb/edu/balamand/uobcalendar/OnCellItemClick.java
@@ -0,0 +1,12 @@
+package lb.edu.balamand.uobcalendar;
+
+/**
+ * Created by Lenovo on 3/25/2015.
+ */
+import android.view.View;
+
+public interface OnCellItemClick {
+
+    public void onCellClick(View v, CardGridItem item);
+
+}
---
+++ app/src/main/java/lb/edu/balamand/uobcalendar/OnItemRender.java
@@ -0,0 +1,10 @@
+package lb.edu.balamand.uobcalendar;
+
+/**
+ * Created by Lenovo on 3/25/2015.
+ */
+public interface OnItemRender {
+
+    public void onRender(CheckableLayout v, CardGridItem item);
+
+}
