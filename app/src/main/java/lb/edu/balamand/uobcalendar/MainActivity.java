08a07fcddadc6a8162e6e08cd1ff031d44452dfb
ali_m91 <ali_m91@live.com>
2015-03-26 17:43:03 +0200

    styles and colors

--- app/src/main/java/lb/edu/balamand/uobcalendar/MainActivity.java
+++ app/src/main/java/lb/edu/balamand/uobcalendar/MainActivity.java
@@ -1,38 +1,17 @@
 package lb.edu.balamand.uobcalendar;
 
-import android.app.Notification;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
-
-import java.util.List;
 
 public class MainActivity extends Activity {
-
-    ListView listView;
-    List<Item> arrayOfList;
-    private static final String rssFeed = "https://dl.dropboxusercontent.com/u/63596028/ProjectNFC/listdata.xml";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // setTitle(R.string.title);
-
-        //listView = (ListView) findViewById(R.id.listview);
-        //listView.setOnItemClickListener(this);
-
-        //LoadItemsAsync();
 
     }
 
